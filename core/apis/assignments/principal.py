6650c62f179b4683a439af9e521b6737b37be9a0
abhishek singh mahra <100080397+Namechangedd@users.noreply.github.com>
2024-09-24 22:38:55 +0530

    added: API for Grade or re-grade an assignment

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -31,3 +31,31 @@ def list_principal_teachers(p):
     teachers_dump = TeacherSchema().dump(all_teachers, many=True)
 
     return APIResponse.respond(data=teachers_dump)
+
+@principal_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
+@decorators.accept_payload
+@decorators.authenticate_principal
+def grade_assignment(p, incoming_payload):
+    """Grade or re-grade an assignment"""
+    
+    assignment_id = incoming_payload.get('id')
+    new_grade = incoming_payload.get('grade')
+    assignment  = Assignment.get_by_id(assignment_id)
+
+
+    if assignment.state == AssignmentStateEnum.DRAFT:
+        return APIResponse.error(message='Assignment in DRAFT state cannot be graded', status_code=400)
+
+    assertions.assert_found(assignment, 'No assignment with this ID was found')
+
+    updated_assignment = Assignment.mark_grade(
+        _id = assignment_id,
+        grade = new_grade,
+        auth_principal=p
+    )
+
+    db.session.commit()
+
+    updated_assignment_dump = AssignmentSchema().dump(updated_assignment)
+
+    return APIResponse.respond(data = updated_assignment_dump)
